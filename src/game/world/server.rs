289db7e4b39775bd310262c88da802eecd54b419
Adam Harmansky <micv@firemail.cc>
2022-05-29 08:12:19 +0200

    Added a halt message to the chunk geenrator thread, removed the media directory

--- src/game/world/server.rs
+++ src/game/world/server.rs
@@ -3,6 +3,11 @@ use std::sync::Arc;
 
 use super::*;
 
+enum ChunkGeneratorMessage {
+    UpdateChunk(BlockPos),
+    Exit,
+}
+
 struct ChunkUpdateInfo {
     mesh: MeshData,
     chunk: Box<Chunk>,
@@ -32,17 +37,27 @@ pub struct ChunkServer {
     // Position of the camera (or anything else loading the chunks)
     pos: BlockPos,
 
-    generator_input: mpsc::Sender<BlockPos>,
+    generator_input: mpsc::Sender<ChunkGeneratorMessage>,
     generator_output: mpsc::Receiver<ChunkUpdateInfo>,
 
     pub block_manager: Arc<BlockManager>,
 }
 
-impl<'a> ChunkServer {
+impl Drop for ChunkServer {
+    fn drop(&mut self) {
+        self.generator_input
+            .send(ChunkGeneratorMessage::Exit)
+            .unwrap();
+        println!("Halting chunk generator thread!");
+        self.recv();
+    }
+}
+
+impl ChunkServer {
     /// Creates a new chunk server, initializes a secondary
     /// chunk generation thread.
     pub fn new(texture: Rc<Texture>, block_manager: Arc<BlockManager>) -> Self {
-        let (our_generator_input, its_generator_input) = mpsc::channel::<BlockPos>();
+        let (our_generator_input, its_generator_input) = mpsc::channel::<ChunkGeneratorMessage>();
         let (its_generator_output, our_generator_output) = mpsc::channel::<ChunkUpdateInfo>();
         let clone = block_manager.clone();
         thread::spawn(move || {
@@ -60,20 +75,27 @@ impl<'a> ChunkServer {
     }
 
     fn run_generator(
-        input: mpsc::Receiver<BlockPos>,
+        input: mpsc::Receiver<ChunkGeneratorMessage>,
         output: mpsc::Sender<ChunkUpdateInfo>,
         manager: Arc<BlockManager>,
     ) {
         loop {
             let msg = input.recv().unwrap();
-            let mut chunk = Box::new(Chunk::new(msg, &manager));
-            let mesh = chunk.update(&manager);
-            output.send(ChunkUpdateInfo { chunk, mesh }).unwrap();
+            match msg {
+                ChunkGeneratorMessage::UpdateChunk(p) => {
+                    let mut chunk = Box::new(Chunk::new(p, &manager));
+                    let mesh = chunk.update(&manager);
+                    output.send(ChunkUpdateInfo { chunk, mesh }).unwrap();
+                }
+                ChunkGeneratorMessage::Exit => break,
+            }
         }
     }
 
     fn request_generation(&self, pos: BlockPos) {
-        self.generator_input.send(pos).unwrap();
+        self.generator_input
+            .send(ChunkGeneratorMessage::UpdateChunk(pos))
+            .unwrap();
     }
 
     /// Load new and unload old chunks, can be safely called every frame
@@ -132,6 +154,21 @@ impl<'a> ChunkServer {
     fn try_recv(&mut self, count: i32) {
         for _ in 0..count {
             let message = self.generator_output.try_recv();
+            let message = if let Ok(message) = message {
+                message
+            } else {
+                break;
+            };
+            self.handle_received_chunk(message);
+        }
+    }
+
+    /// Receive all chunks, useful when halting the generator
+    ///
+    /// Runs until there is an error
+    fn recv(&mut self) {
+        loop {
+            let message = self.generator_output.recv();
             let message = if let Ok(message) = message {
                 message
             } else {
